0ccc3e19fea7351d9be2cb956164bc4b9bc25b0e
Brendo Raposo <raposobrendo@gmail.com>
2024-10-23 14:24:58 -0300

    Bank/account 1.2

--- constThisOverEncapExercises/exercicios/Account.java
+++ constThisOverEncapExercises/exercicios/Account.java
@@ -4,41 +4,46 @@ public class Account {
 	private final int account;
 	private String holder;
 	private double balance;
-	
-	public Account(int account, String holder, double balance) {
+
+	public Account(int account, String holder, double initialDeposit) {
 		this.account = account;
 		this.holder = holder;
-		this.balance = balance;
+		depositValue(initialDeposit);
 	}
-	
+
 	public Account(int account, String holder) {
 		this.account = account;
 		this.holder = holder;
 	}
 	
+	public int account() {
+		return account;
+	}
+
 	public void setHolder(String holder) {
 		this.holder = holder;
 	}
 	
-	public double getBalance(){
-		return balance;
-	}
 	public String getHolder(String holder) {
 		return holder;
+	}
+
+	public double getBalance() {
+		return balance;
 	}
 	
 	public void initialDeposit(double deposit) {
 		balance += deposit;
 	}
-	
+
 	public void depositValue(double deposit) {
 		balance += deposit;
 	}
-	
+
 	public void withdrawValue(double withdraw) {
 		balance -= withdraw + 5;
 	}
-	
+
 	public String toString() {
 		return "Account " + account + " , Holder: " + holder + ", Balance: " + balance;
 	}
